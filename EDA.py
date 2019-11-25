71ae730a6f2db52c4c1b5fe1facdaff25b7967d5
jeanjean1 <33195649+jeanjean1@users.noreply.github.com>
2019-11-25 11:13:13 +0900

    added ETS

---
+++ EDA.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+fname =  open('fname.txt','r').read()
+df = pd.read_csv(fname, index_col=0, parse_dates=True) # Insert your own filename
+
+# Seasonality
+from statsmodels.tsa.seasonal import seasonal_decompose
+frequencies = [60*24,60*24*7 , 60*24*30] # daily, weekly, monthly
+for freq in frequencies:
+    result = seasonal_decompose(df.consumption, model='additive', freq = freq)
+    result.plot()
+
+
+    
